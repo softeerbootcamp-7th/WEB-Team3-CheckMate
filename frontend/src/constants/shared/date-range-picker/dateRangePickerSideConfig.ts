3d607c03e222f1629fbd4815248ff6f6fb3334c7
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-26 01:19:38 +0900

    feat: date range picker 타입 별 상수 정의

---
+++ frontend/src/constants/shared/date-range-picker/dateRangePickerSideConfig.ts
@@ -0,0 +1,62 @@
+import { formatDateYYYYMM, formatDateYYYYMMDD } from '@/utils/shared';
+
+import { DATE_RANGE_PICKER_TYPE } from './dateRangePickerType';
+
+export const DATE_RANGE_SIDE_CONFIG = {
+  [DATE_RANGE_PICKER_TYPE.date]: {
+    alertText: '최대 30일까지 선택할 수 있어요.',
+    validate: (startDate?: Date, endDate?: Date) => {
+      if (!startDate || !endDate) {
+        return true;
+      }
+
+      const dateDifference =
+        (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
+
+      return dateDifference <= 30;
+    },
+    formattedDate: formatDateYYYYMMDD,
+  },
+  [DATE_RANGE_PICKER_TYPE.week]: {
+    alertText: '최대 12주까지 선택할 수 있어요.',
+    validate: (startDate?: Date, endDate?: Date) => {
+      if (!startDate || !endDate) {
+        return true;
+      }
+
+      const dateDifference =
+        (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
+
+      // 7 * 12 = 84
+      return dateDifference <= 84;
+    },
+    formattedDate: formatDateYYYYMM,
+  },
+  [DATE_RANGE_PICKER_TYPE.month]: {
+    alertText: '최대 12개월까지 선택할 수 있어요.',
+    validate: (startDate?: Date, endDate?: Date) => {
+      if (!startDate || !endDate) {
+        return true;
+      }
+
+      const yearDifference = endDate.getFullYear() - startDate.getFullYear();
+      const monthDifference = endDate.getMonth() - startDate.getMonth();
+
+      return yearDifference * 12 + monthDifference <= 12;
+    },
+    formattedDate: formatDateYYYYMM,
+  },
+  [DATE_RANGE_PICKER_TYPE.year]: {
+    alertText: '최대 3년까지 선택할 수 있어요.',
+    validate: (startDate?: Date, endDate?: Date) => {
+      if (!startDate || !endDate) {
+        return true;
+      }
+
+      const yearDifference = endDate.getFullYear() - startDate.getFullYear();
+
+      return yearDifference <= 3;
+    },
+    formattedDate: formatDateYYYYMM,
+  },
+} as const;
