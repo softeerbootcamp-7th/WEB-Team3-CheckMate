335e9e3be83350817428c9cf2454a85f86afea9e
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-26 01:06:25 +0900

    feat: date range picker 시작 - 끝 관련 indicator 컴포넌트 추가

---
+++ frontend/src/components/shared/date-range-picker/DateRangePicker.start-end-indicator.tsx
@@ -0,0 +1,9 @@
+export const DateRangePickerStartEndIndicator = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="bg-grey-400 size-1.5 rounded-full" />
+      <div className="border-grey-400 h-12.5 w-0.25 border-l border-dashed" />
+      <div className="bg-grey-400 size-1.5 rounded-full" />
+    </div>
+  );
+};
