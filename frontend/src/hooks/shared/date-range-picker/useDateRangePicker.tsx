8f0abe113e4fc068008bfccdc1857171001278e1
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-26 12:05:41 +0900

    refactor: 파일명 middle name에서 파스칼 케이스로 변경

---
+++ frontend/src/hooks/shared/date-range-picker/useDateRangePicker.tsx
@@ -0,0 +1,70 @@
+import { useCallback, useMemo, useState } from 'react';
+
+import {
+  DATE_RANGE_PICKER_TYPE,
+  type DateRangePickerType,
+} from '@/constants/shared';
+
+interface UseDateRangePickerProps {
+  startDate?: Date;
+  endDate?: Date;
+  setStartDate?: (date?: Date) => void;
+  setEndDate?: (date?: Date) => void;
+  dateRangePickerType: DateRangePickerType;
+}
+
+export const useDateRangePicker = ({
+  startDate,
+  endDate,
+  setStartDate,
+  setEndDate,
+  dateRangePickerType,
+}: UseDateRangePickerProps) => {
+  const [selectedStartDate, setSelectedStartDate] = useState<Date | undefined>(
+    startDate,
+  );
+  const [selectedEndDate, setSelectedEndDate] = useState<Date | undefined>(
+    endDate,
+  );
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const ariaLabel = useMemo(() => {
+    switch (dateRangePickerType) {
+      case DATE_RANGE_PICKER_TYPE.date:
+        return '날짜 범위 선택';
+      case DATE_RANGE_PICKER_TYPE.week:
+        return '주 범위 선택';
+      case DATE_RANGE_PICKER_TYPE.month:
+        return '월 범위 선택';
+      case DATE_RANGE_PICKER_TYPE.year:
+        return '년 범위 선택';
+      default:
+        return '기간 선택';
+    }
+  }, [dateRangePickerType]);
+
+  const handleCancel = useCallback(() => {
+    setSelectedStartDate(undefined);
+    setSelectedEndDate(undefined);
+    setIsOpen(false);
+  }, []);
+
+  const handleSave = useCallback(() => {
+    setStartDate?.(selectedStartDate);
+    setEndDate?.(selectedEndDate);
+    setIsOpen(false);
+  }, [selectedStartDate, selectedEndDate, setStartDate, setEndDate]);
+
+  return {
+    isOpen,
+    setIsOpen,
+    ariaLabel,
+    handleCancel,
+    handleSave,
+    selectedStartDate,
+    selectedEndDate,
+    setSelectedStartDate,
+    setSelectedEndDate,
+  };
+};
