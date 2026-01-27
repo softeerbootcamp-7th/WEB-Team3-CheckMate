403b8fc2b6a4d2415cc012ea3b3640844a9c77c9
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-27 11:30:26 +0900

    feat: 월 단위 기간 설정 캘린더 cell grid 추가

---
+++ frontend/src/components/shared/calendar/CalendarMonthGrid.tsx
@@ -0,0 +1,72 @@
+import {
+  cn,
+  getCurrentMonth,
+  isBetweenSelectedDate,
+  isEndDate,
+  isStartDate,
+} from '@/utils/shared';
+
+import { CalendarMonthCell } from './CalendarMonthCell';
+
+interface CalendarMonthGridProps {
+  currentDateForCalendar: Date;
+  selectedStartDate?: Date;
+  selectedEndDate?: Date;
+  lastWeekOfPreviousMonth: number[];
+  numberOfDatesForCalendar: number;
+  firstWeekOfNextMonth: number[];
+  handleSelectMonth: (currentDate: Date) => void;
+}
+
+export const CalendarMonthGrid = ({
+  currentDateForCalendar,
+  selectedStartDate,
+  selectedEndDate,
+  handleSelectMonth,
+}: CalendarMonthGridProps) => {
+  const renderMonthCell = ({ month }: { month: number }) => {
+    const currentDate = getCurrentMonth({
+      month: month - 1,
+      dateForCalendar: currentDateForCalendar,
+    });
+
+    const isStart = isStartDate({
+      currentDate,
+      selectedStartDate,
+    });
+
+    const isEnd = isEndDate({
+      currentDate,
+      selectedEndDate,
+    });
+
+    const isBetweenStartEndDate = isBetweenSelectedDate({
+      currentDate,
+      selectedStartDate,
+      selectedEndDate,
+    });
+
+    const isSelected = isStart || isEnd;
+    return (
+      <CalendarMonthCell
+        key={month}
+        month={month}
+        className={cn(
+          isSelected &&
+            'before:bg-grey-900 text-grey-50 before:rounded-unlimit after:bg-grey-100 size-full before:absolute before:inset-0 before:z-2 before:content-[""] after:absolute after:z-1 after:h-full after:w-1/2',
+          isStart && 'after:right-0',
+          isEnd && 'after:left-0',
+          isBetweenStartEndDate && 'bg-grey-100',
+        )}
+        onClick={() => handleSelectMonth(currentDate)}
+      />
+    );
+  };
+  return (
+    <div className="grid grid-cols-3 gap-y-3">
+      {Array.from({ length: 12 }).map((_, index) =>
+        renderMonthCell({ month: index + 1 }),
+      )}
+    </div>
+  );
+};
