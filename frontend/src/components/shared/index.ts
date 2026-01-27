03d6fc1e8fe840cf86c1a91d64cf520bff64c08d
lwjmcn <clarecse02@gmail.com>
2026-01-27 16:20:29 +0900

    chore: 배럴 파일 추가

--- frontend/src/components/shared/index.ts
+++ frontend/src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { Sidebar } from './sidebar';
 export { MainLayout } from './main-layout';
 export { DateRangePicker } from './date-range-picker';
 export { Calendar } from './calendar';
+export { SectionTitle } from './section-title';
