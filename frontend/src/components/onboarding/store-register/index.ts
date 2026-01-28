fbb1f7db1dd055a9751f374045064f9c14cd9310
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-29 01:54:42 +0900

    feat: 매장 등록 Form 임시 컴포넌트 추가

---
+++ frontend/src/components/onboarding/store-register/index.ts
@@ -0,0 +1,2 @@
+export { StoreRegisterSidebar } from './store-register-sidebar';
+export { StoreRegisterForm } from './store-register-form';
---
+++ frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
@@ -0,0 +1,7 @@
+export const StoreRegisterForm = () => {
+  return (
+    <div>
+      <h1>Store Register Form</h1>
+    </div>
+  );
+};
