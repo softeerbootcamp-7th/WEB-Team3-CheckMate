aa38f6e1e639b8551c16d4a2f359d4b5bae054cf
jho7535 <jho7535@naver.com>
2026-01-28 21:34:41 +0900

    feat: email 컬럼 추가

--- backend/src/main/java/com/checkmate/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/checkmate/backend/domain/member/entity/Member.java
@@ -1,16 +1,22 @@
 package com.checkmate.backend.domain.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
 @Table(name = "member")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "member_id")
   private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String email;
 }
