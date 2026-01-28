f0362edd478baca413bff825db926eceab5f858a
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-28 15:41:29 +0900

    feat: 온보딩 페이지 컴포넌트 추가

---
+++ frontend/src/pages/on-boarding-page/OnBoardingPage.tsx
@@ -0,0 +1,10 @@
+import { OnboardingLayout } from '@/components/shared';
+
+export const OnBoardingPage = () => {
+  return (
+    <OnboardingLayout>
+      <OnboardingLayout.Sidebar>sidebar</OnboardingLayout.Sidebar>
+      <OnboardingLayout.Main>main</OnboardingLayout.Main>
+    </OnboardingLayout>
+  );
+};
---
+++ frontend/src/pages/on-boarding-page/index.ts
@@ -0,0 +1,1 @@
+export { OnBoardingPage } from './OnBoardingPage';
