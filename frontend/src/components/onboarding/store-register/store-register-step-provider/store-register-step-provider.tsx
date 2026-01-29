442b70ce79b8f08cb097f709c24a8dadd9de623b
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-29 16:36:39 +0900

    feat: 다음 스텝 이동, 이전 스텝 이동 action 추가

--- frontend/src/components/onboarding/store-register/store-register-step-provider/store-register-step-provider.tsx
+++ frontend/src/components/onboarding/store-register/store-register-step-provider/store-register-step-provider.tsx
@@ -1,11 +1,27 @@
-import { type PropsWithChildren, useMemo, useState } from 'react';
+import { type PropsWithChildren, useCallback, useMemo, useState } from 'react';
 
-import { storeRegisterStepContext } from '@/constants/onboarding/store-register';
+import {
+  STORE_REGISTER_STEP,
+  storeRegisterStepContext,
+} from '@/constants/onboarding/store-register';
 
 export const StoreRegisterStepProvider = ({ children }: PropsWithChildren) => {
-  const [currentStep, setCurrentStep] = useState<number>(1);
+  const [currentStep, setCurrentStep] = useState<number>(
+    STORE_REGISTER_STEP.BUSINESS_REGISTRATION_NUMBER,
+  );
 
-  const value = useMemo(() => ({ currentStep, setCurrentStep }), [currentStep]);
+  const moveNextStep = useCallback(() => {
+    setCurrentStep((prev) => prev + 1);
+  }, []);
+
+  const movePreviousStep = useCallback(() => {
+    setCurrentStep((prev) => prev - 1);
+  }, []);
+
+  const value = useMemo(
+    () => ({ currentStep, moveNextStep, movePreviousStep }),
+    [currentStep, moveNextStep, movePreviousStep],
+  );
 
   return (
     <storeRegisterStepContext.Provider value={value}>
