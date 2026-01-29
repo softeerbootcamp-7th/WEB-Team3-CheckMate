f73db270e8cf7ff003a92fc8ab2ed291340b9edb
jho7535 <jho7535@naver.com>
2026-01-29 10:20:40 +0900

    feat: 멤버 토큰 저장 엔터티 추가
    - Google Access Token 및 Refresh Token, 서버 Refresh Token을 저장
    - Member 엔티티와 1:1 관계를 설정

---
+++ backend/src/main/java/com/checkmate/backend/domain/member/entity/MemberAuth.java
@@ -0,0 +1,57 @@
+package com.checkmate.backend.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+    name = "member_auth",
+    indexes = {@Index(name = "idx_member_id", columnList = "member_id")})
+public class MemberAuth {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_auth_id")
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+      name = "member_id",
+      foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)
+      )
+  private Member member;
+
+  @Column(columnDefinition = "TEXT")
+  @Convert(converter = TokenEncryptionConverter.class)
+  private String googleAccessToken;
+
+  @Column(columnDefinition = "TEXT")
+  @Convert(converter = TokenEncryptionConverter.class)
+  private String googleRefreshToken;
+
+  @Column(columnDefinition = "TEXT")
+  private String refreshToken;
+
+  public void updateGoogleTokens(String accessToken, String refreshToken) {
+    this.googleAccessToken = accessToken;
+    if (refreshToken != null) {
+      this.googleRefreshToken = refreshToken;
+    }
+  }
+
+  public void updateRefreshToken(String ourRefreshToken) {
+    this.refreshToken = ourRefreshToken;
+  }
+
+  // 정적 팩토리 메서드 사용
+  public static MemberAuth createWithMember(Member member) {
+    MemberAuth memberAuth = new MemberAuth();
+    memberAuth.member = member;
+    return memberAuth;
+  }
+}
