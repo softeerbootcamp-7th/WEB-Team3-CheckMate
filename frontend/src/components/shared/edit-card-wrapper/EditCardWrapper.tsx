757ad8b7edb79dc80fac5c7cc501d95b2606551b
minseon <mskwon0601@gmail.com>
2026-01-29 15:46:43 +0900

    feat: 대시보드 편집용 패널로 만들어주는 카드 wrapper 구현

---
+++ frontend/src/components/shared/edit-card-wrapper/EditCardWrapper.tsx
@@ -0,0 +1,110 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
+import { CircleCheck } from 'lucide-react';
+
+import { cn } from '@/utils/shared';
+
+import { PeriodTag } from './PeriodTag';
+import { PlusIconButton } from './PlusIconButton';
+import { TrashCanIconButton } from './TrashCanIconButton';
+
+// 대시보드 편집 패널 카드로 만들어주는 wrapper 컴포넌트
+// child에는 순수한 카드 내용만 넣어준다
+interface EditCardWrapperProps {
+  isAdded: boolean;
+  children: React.ReactNode;
+  className?: string; // 전체 wrapper 크기나 보더 등 스타일
+  period: string; // 오늘, 이번주, 이번달 등 문구
+
+  onClickDeleteButton?: () => void; // 대시보드에서 삭제하는 버튼 클릭 헨들러
+  onClickAddButton?: () => void; // 대시보드에 추가하는 버튼 클릭 핸들러
+}
+
+const SHRINK_SCALE = 0.65; // 65% 축소
+const PADDING_SIZE = 12; // 패딩 박스 내 여백 사이즈(12px)
+const HEADER_HEIGHT = 26; // 헤더 높이(위에 있는 버튼들 높이 : 26px)
+const HEATHER_MAIN_GAP = 16; // 헤더와 내용 사이 간격(16px)
+
+export const EditCardWrapper = ({
+  isAdded,
+  children,
+  className,
+  period = '기간없음',
+  onClickDeleteButton,
+  onClickAddButton,
+}: EditCardWrapperProps) => {
+  const childRef = useRef<HTMLDivElement | null>(null); // 자식 요소 dom에서 잡기용
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    if (childRef.current) {
+      const { width, height } = {
+        width: childRef.current.clientWidth,
+        height: childRef.current.clientHeight,
+      };
+
+      setSize({ width, height });
+    }
+  }, []);
+
+  // 전체 카드 크기 계산
+  // 카드 가로 크기 = (자식 컴포넌트 가로 크기 * 축소 비율) + 왼쪽 오른쪽 여백
+  const computedChildWidth =
+    Math.floor(size.width * SHRINK_SCALE) + PADDING_SIZE * 2;
+  // 카드 세로 크기 = (자식 컴포넌트 세로 크기 * 축소 비율) + 위쪽 아래 여백 + 헤더높이 + 헤더와 내용 사이 여백
+  const computedChildHeight =
+    Math.floor(size.height * SHRINK_SCALE) +
+    PADDING_SIZE * 2 +
+    HEADER_HEIGHT +
+    HEATHER_MAIN_GAP;
+
+  return (
+    <div
+      style={{
+        width: Math.max(220, computedChildWidth), // 최소 너비 220px
+        height: Math.max(147, computedChildHeight), // 최소 높이 147px
+      }}
+      className={cn(
+        'bg-special-card-bg rounded-400 relative flex flex-col overflow-hidden border border-gray-300 p-3',
+        className,
+      )}
+    >
+      <div className="flex justify-between">
+        <PeriodTag isAdded={isAdded} period={period} />
+        {isAdded ? (
+          <TrashCanIconButton onClickDeleteButton={onClickDeleteButton} />
+        ) : (
+          <PlusIconButton onClickAddButton={onClickAddButton} />
+        )}
+      </div>
+      <div
+        style={{ marginTop: `${HEATHER_MAIN_GAP}px` }}
+        className="flex min-w-0 flex-1 items-end justify-center"
+      >
+        <div
+          style={{
+            transform: `scale(${SHRINK_SCALE})`,
+            transformOrigin: computedChildHeight < 147 ? 'center' : 'top',
+          }}
+          ref={childRef}
+          className={cn(isAdded ? 'opacity-10' : 'opacity-100')}
+        >
+          {children}
+        </div>
+      </div>
+
+      {isAdded && (
+        <div className="pointer-events-none absolute inset-[10px] flex items-center justify-center">
+          <button
+            className={cn(
+              'caption-large-semibold rounded-unlimit flex gap-1 border border-gray-200 bg-gray-100 py-1 pr-2 pl-[10px] text-center text-gray-900',
+            )}
+          >
+            대시보드 추가
+            <CircleCheck className="size-4 fill-gray-900 text-gray-50" />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ frontend/src/components/shared/edit-card-wrapper/TrashCanIconButton.tsx
@@ -0,0 +1,19 @@
+import { Trash2 } from 'lucide-react';
+
+// 대시보드 편집 용 패널의 우측 위 쓰래기통 버튼
+interface TrashCanIconButtonProps {
+  onClickDeleteButton?: () => void;
+}
+
+export const TrashCanIconButton = ({
+  onClickDeleteButton,
+}: TrashCanIconButtonProps) => {
+  return (
+    <button
+      onClick={onClickDeleteButton}
+      className="rounded-unlimit hover:text-others-negative flex size-[26px] items-center justify-center border border-gray-200 bg-gray-100 text-gray-900 active:bg-gray-900 active:text-gray-50"
+    >
+      <Trash2 className="size-4" />
+    </button>
+  );
+};
