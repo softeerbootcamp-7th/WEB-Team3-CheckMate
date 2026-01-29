ac6fd88041f1c4e1062a612d6d000e2de0aaf370
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-29 18:37:22 +0900

    feat: 매장 정보 입력 section 추가

---
+++ frontend/src/components/onboarding/store-register/store-info-input-section/StoreInfoInputSection.tsx
@@ -0,0 +1,27 @@
+import { NextStepButton } from '../next-step-button';
+import { PreviousStepButton } from '../previous-step-button';
+import { StoreAddressInput } from '../store-address-input';
+import { StoreNameInput } from '../store-name-input';
+import { StoreRegisterFormTitle } from '../store-register-form-title';
+
+export const StoreInfoInputSection = () => {
+  return (
+    <section className="flex flex-col items-center pt-32">
+      <div className="flex w-95 flex-col gap-12">
+        <StoreRegisterFormTitle
+          title={`매장 정보를 입력하면 \n 맞춤형 서비스를 제공해요`}
+        />
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-9">
+            <StoreNameInput />
+            <StoreAddressInput />
+          </div>
+          <div className="flex items-center gap-4 self-end">
+            <PreviousStepButton />
+            <NextStepButton />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- frontend/src/components/onboarding/store-register/store-register-form-content/StoreRegisterFormContent.tsx
+++ frontend/src/components/onboarding/store-register/store-register-form-content/StoreRegisterFormContent.tsx
@@ -2,12 +2,15 @@ import { STORE_REGISTER_STEP } from '@/constants/onboarding/store-register';
 import { useStoreRegisterStepContext } from '@/hooks/onboarding/store-register';
 
 import { BusinessRegistrationNumberInputSection } from '../business-registration-number-input-section';
+import { StoreInfoInputSection } from '../store-info-input-section';
 
 export const StoreRegisterFormContent = () => {
   const { currentStep } = useStoreRegisterStepContext();
   switch (currentStep) {
     case STORE_REGISTER_STEP.BUSINESS_REGISTRATION_NUMBER:
       return <BusinessRegistrationNumberInputSection />;
+    case STORE_REGISTER_STEP.STORE_INFORMATION:
+      return <StoreInfoInputSection />;
     default:
       return <></>;
   }
