a7269acf631b257ca606695c87941ba17903c571
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-29 15:03:53 +0900

    feat: step 진행도를 알려주는 컴포넌트명을 stepIndicator에서 stepper로 변경

---
+++ frontend/src/components/onboarding/store-register/store-register-stepper/StoreRegisterStep.tsx
@@ -0,0 +1,37 @@
+import { useStoreRegisterStepContext } from '@/hooks/onboarding/store-register';
+import type { StoreRegisterStep as StoreRegisterStepType } from '@/types/onboarding/store-register';
+import { cn } from '@/utils/shared';
+
+interface StoreRegisterStepProps {
+  step: StoreRegisterStepType['step'];
+  label: StoreRegisterStepType['label'];
+}
+
+export const StoreRegisterStep = ({ step, label }: StoreRegisterStepProps) => {
+  const { currentStep } = useStoreRegisterStepContext();
+
+  const isActiveOrDone = step <= currentStep;
+  return (
+    <div className="flex items-center gap-3">
+      <span
+        className={cn(
+          'rounded-100 body-large-semibold flex size-7.5 items-center justify-center',
+          isActiveOrDone
+            ? 'bg-others-50 text-brand-main'
+            : 'bg-grey-200 text-grey-700',
+        )}
+      >
+        {step}
+      </span>
+      <span
+        className={cn(
+          isActiveOrDone
+            ? 'text-brand-main body-large-bold'
+            : 'text-grey-600 body-large-medium',
+        )}
+      >
+        {label}
+      </span>
+    </div>
+  );
+};
