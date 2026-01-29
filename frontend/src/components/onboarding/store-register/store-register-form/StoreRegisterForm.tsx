eb490cad9d3b500eae801a6a3fd56fbe2dfa977f
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-29 16:38:52 +0900

    feat: 다음 스텝으로 이동하기 로직 추가

--- frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
+++ frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
@@ -1,7 +1,26 @@
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { STORE_REGISTER_STEP } from '@/constants/onboarding/store-register';
+import { useStoreRegisterStepContext } from '@/hooks/onboarding/store-register';
+
+import { StoreRegisterFormContent } from '../store-register-form-content';
+
 export const StoreRegisterForm = () => {
+  // TODO: 폼 type 타입 정의하기
+  const methods = useForm();
+  const { currentStep, moveNextStep } = useStoreRegisterStepContext();
+
+  const handleSubmit = methods.handleSubmit(() => {
+    if (currentStep < STORE_REGISTER_STEP.SALES_CLOSING_TIME) {
+      moveNextStep();
+    }
+  });
+
   return (
-    <div>
-      <h1>Store Register Form</h1>
-    </div>
+    <FormProvider {...methods}>
+      <form className="size-full" onSubmit={handleSubmit}>
+        <StoreRegisterFormContent />
+      </form>
+    </FormProvider>
   );
 };
