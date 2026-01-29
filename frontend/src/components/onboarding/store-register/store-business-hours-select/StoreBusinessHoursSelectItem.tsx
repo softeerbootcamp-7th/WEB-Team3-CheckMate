066a82b6f45f06c82df8a50354fa5f41561ae757
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-30 02:05:46 +0900

    feat: 매장 영업 시간 select 컴포넌트 추가
    - select
    - select-trigger
    - select-item으로 세분화하여 구현

---
+++ frontend/src/components/onboarding/store-register/store-business-hours-select/StoreBusinessHoursSelectItem.tsx
@@ -0,0 +1,32 @@
+import { memo } from 'react';
+
+import { SelectItem } from '@/components/shared/shadcn-ui';
+import { FINAL_STORE_BUSINESS_HOURS_TIME } from '@/constants/onboarding/store-register';
+import { cn } from '@/utils/shared';
+
+interface StoreBusinessHoursSelectItemProps {
+  hour: number;
+  minute: number;
+}
+
+export const StoreBusinessHoursSelectItem = memo(
+  ({ hour, minute }: StoreBusinessHoursSelectItemProps) => {
+    const { hour: finalHour, minute: finalMinute } =
+      FINAL_STORE_BUSINESS_HOURS_TIME;
+    const formattedTime = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+    return (
+      <SelectItem
+        value={formattedTime}
+        className={cn(
+          'body-medium-medium text-grey-700 bg-grey-100 hover:bg-grey-300 active:bg-grey-500 active:text-grey-50 flex h-9 cursor-pointer items-center justify-center rounded-none',
+          !(hour === finalHour && minute === finalMinute) &&
+            'border-grey-300 border-b',
+        )}
+      >
+        {formattedTime}
+      </SelectItem>
+    );
+  },
+);
+
+StoreBusinessHoursSelectItem.displayName = 'StoreBusinessHoursSelectItem';
