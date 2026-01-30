bbe9debafe43f46dd2ab6f535490064e4331c537
minseon <mskwon0601@gmail.com>
2026-01-30 12:28:38 +0900

    chore: 배럴파일로 export import

---
+++ frontend/src/types/weather/forecast/index.ts
@@ -0,0 +1,4 @@
+export { type WeatherForecastToday } from './WeatherForecastToday';
+export { type WeatherForecastOneDayHalf } from './WeatherForecastOneDayHalf';
+export { type WeatherForecastOneDayAmPm } from './WeatherForecastOneDayAmPm';
+export { type WeatherForecastHour } from './WeatherForecastHour';
