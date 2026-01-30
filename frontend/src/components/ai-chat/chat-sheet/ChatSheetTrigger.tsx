1ea258e26d2599bbddfde571b370fafae7d8d6e2
lwjmcn <clarecse02@gmail.com>
2026-01-30 19:02:14 +0900

    feat: 사이드바에 챗봇 버튼 추가

--- frontend/src/components/ai-chat/chat-sheet/ChatSheetTrigger.tsx
+++ frontend/src/components/ai-chat/chat-sheet/ChatSheetTrigger.tsx
@@ -4,7 +4,7 @@ import { CDN_BASE_URL } from '@/constants/shared';
 
 export const ChatSheetTrigger = () => {
   return (
-    <SheetTrigger asChild>
+    <SheetTrigger asChild className="fixed bottom-15 left-10">
       <Button className="bg-grey-0 size-18 rounded-full border-none p-350 pb-400 shadow-[0_0_3.6px_0_rgba(0,0,0,0.15),0_3.6px_7.2px_0_rgba(0,0,0,0.08)]">
         <img
           src={`${CDN_BASE_URL}/assets/images/ai_chat_column.png`}
