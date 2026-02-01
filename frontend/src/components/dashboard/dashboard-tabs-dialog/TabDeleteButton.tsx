3203b0fe9ad9bea3fc2a38ba9589ba8ee9f672f9
lwjmcn <clarecse02@gmail.com>
2026-02-02 05:22:27 +0900

    feat: 대시보드 탭 다이얼로그 버튼 컴포넌트 추가
    - 다이얼로그 footer의 전체 취소, 저장 버튼 컴포넌트 구현
    - 다이얼로그 내 5개의 tab name input에 대한 추가, 수정, 삭제 버튼 컴포넌트
    - 버튼 클릭 시 onClick 이벤트 핸들러 전달

---
+++ frontend/src/components/dashboard/dashboard-tabs-dialog/TabDeleteButton.tsx
@@ -0,0 +1,23 @@
+import TrashIcon from '@/assets/icons/trash.svg?react';
+import { Button } from '@/components/shared/shadcn-ui';
+
+interface TabDeleteButtonProps {
+  onClick: () => void;
+  disabled: boolean;
+}
+
+export const TabDeleteButton = ({
+  onClick,
+  disabled,
+}: TabDeleteButtonProps) => {
+  return (
+    <Button
+      onClick={onClick}
+      aria-label="삭제"
+      disabled={disabled}
+      className="p-0!"
+    >
+      <TrashIcon className="text-grey-600 disabled:text-grey-400 size-6" />
+    </Button>
+  );
+};
