da952f3271bdebe244d47331f2ee460cf62c5d02
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-02 23:17:50 +0900

    feat: 영업 마감 시간 관련 리스트 상수로 분리 및 useMemo 제거

--- frontend/src/components/onboarding/store-register/sales-closing-time-select/SalesClosingTimeSelect.tsx
+++ frontend/src/components/onboarding/store-register/sales-closing-time-select/SalesClosingTimeSelect.tsx
@@ -1,15 +1,13 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 
 import { Select, SelectContent } from '@/components/shared/shadcn-ui';
 
 import { SalesClosingTimeSelectItem } from './SalesClosingTimeSelectItem';
 import { SalesClosingTimeSelectTrigger } from './SalesClosingTimeSelectTrigger';
 
-export const SalesClosingTimeSelect = () => {
-  const salesClosingTimeList = useMemo(() => {
-    return Array.from({ length: 24 }, (_, index) => index);
-  }, []);
+const SALES_CLOSING_TIME_LIST = Array.from({ length: 24 }, (_, index) => index);
 
+export const SalesClosingTimeSelect = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const handleOpenChange = (open: boolean) => {
@@ -20,7 +18,7 @@ export const SalesClosingTimeSelect = () => {
     <Select open={isOpen} onOpenChange={handleOpenChange} defaultValue="0시">
       <SalesClosingTimeSelectTrigger isOpen={isOpen} />
       <SelectContent position="popper" className="h-54 border-none">
-        {salesClosingTimeList.map((time) => (
+        {SALES_CLOSING_TIME_LIST.map((time) => (
           <SalesClosingTimeSelectItem key={time} time={time} />
         ))}
       </SelectContent>
