fe88d0ce8dfdcbf69a7f09e7c99e876cd5ad6972
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-02 23:31:40 +0900

    chore: 인라인 스타일링을 사용한 이유 관련 주석 추가

--- frontend/src/components/onboarding/store-register/sales-closing-time-select/SalesClosingTimeSelectTrigger.tsx
+++ frontend/src/components/onboarding/store-register/sales-closing-time-select/SalesClosingTimeSelectTrigger.tsx
@@ -14,6 +14,7 @@ export const SalesClosingTimeSelectTrigger = ({
         'rounded-150 bg-grey-100 body-medium-semibold! flex w-25 cursor-pointer items-center justify-end border-none px-250 py-200 [&_svg]:transition-transform [&_svg]:duration-200',
         isOpen && '[&_svg]:rotate-180',
       )}
+      // tailwindcss outline-none 때문에 클래스 적용이 안돼서 inline style로 적용
       style={{
         outline: isOpen ? '1px solid var(--color-grey-300)' : 'none',
       }}
