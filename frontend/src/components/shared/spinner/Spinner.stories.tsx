4ee695a7f6034ef80557c20092f8ed0d87fbd3dc
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-02 23:18:49 +0900

    chore: spinner 관련 스토리북 description 수정

--- frontend/src/components/shared/spinner/Spinner.stories.tsx
+++ frontend/src/components/shared/spinner/Spinner.stories.tsx
@@ -13,7 +13,7 @@ const meta = {
     className: {
       control: 'text',
       description:
-        'Tailwind CSS 클래스를 추가할 수 있습니다. 부여한 color 속성에 opacity: 0.1이 적용된 색상이 배경색으로 적용됩니다.',
+        'Tailwind CSS 클래스를 추가할 수 있습니다. 전달한 text 색상 클래스는 SVG의 currentColor로 사용되어 스피너의 색상을 결정하며, SVG 내부 일부 path에는 동일한 색상에 opacity: 0.1이 적용되어 옅은 영역이 표현됩니다.',
     },
   },
 } satisfies Meta<typeof Spinner>;
