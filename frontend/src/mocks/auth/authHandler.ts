0c2e2c86567cbdb9e4b53d8fb1fb205c69dde898
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-04 00:50:39 +0900

    feat: auth 관련 mock handler 추가
    - 사용자 정보 요청 api
    - access token 재발급 api.

---
+++ frontend/src/mocks/auth/authHandler.ts
@@ -0,0 +1,62 @@
+import { HttpResponse } from 'msw';
+
+import { type ErrorResponse, type SuccessResponse } from '@/services/shared';
+import type {
+  GetAuthMeResponseDto,
+  PostAuthRefreshResponseDto,
+} from '@/types/auth';
+
+import { mswHttp } from '../shared';
+
+let requestCount = false;
+
+const getHandler = [
+  mswHttp.get('/auth/me', () => {
+    // access token 갱신 테스트를 위해 첫 요청은 401 응답
+    if (!requestCount) {
+      requestCount = true;
+      return HttpResponse.json<ErrorResponse>(
+        {
+          success: false,
+          message: 'Unauthorized',
+          errorCode: 'UNAUTHORIZED',
+        },
+        { status: 401 },
+      );
+    }
+    return HttpResponse.json<SuccessResponse<GetAuthMeResponseDto>>(
+      {
+        success: true,
+        data: {
+          userInfo: {
+            id: 1,
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+          },
+          onboaradingStatus: 'NONE',
+          storeInfo: {
+            storeId: 1,
+            storeName: 'Mock Store',
+          },
+        },
+      },
+      {
+        status: 200,
+      },
+    );
+  }),
+];
+
+const postHandler = [
+  mswHttp.post('/auth/refresh', () =>
+    HttpResponse.json<SuccessResponse<PostAuthRefreshResponseDto>>(
+      {
+        success: true,
+        data: { accessToken: 'mock-access-token' },
+      },
+      { status: 200 },
+    ),
+  ),
+];
+
+export const authHandler = [...getHandler, ...postHandler];
