27627686ac9ac291586a6a40310833d7c25342f8
jho7535 <jho7535@naver.com>
2026-02-03 19:20:39 +0900

    chore: CORS 오리진 추가

--- backend/src/main/java/com/checkmate/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/checkmate/backend/global/config/WebConfig.java
@@ -12,8 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
                         "https://check-mate.kro.kr",
-                        "https://localhost:5173",
-                        "http://localhost:5173")
+                        "http://check-mate.kro.kr",
+                        "https://localhost:[*]",
+                        "http://localhost:[*]")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키(Refresh Token) 주고받으려면 필수
