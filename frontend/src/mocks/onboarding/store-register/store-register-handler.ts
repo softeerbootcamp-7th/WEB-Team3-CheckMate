b2c17d911d77dc06d99d7d51dc9cf81437d81aa0
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-04 00:55:14 +0900

    chore: 매장 등록 msw handler에서 post handler 분리

--- frontend/src/mocks/onboarding/store-register/store-register-handler.ts
+++ frontend/src/mocks/onboarding/store-register/store-register-handler.ts
@@ -4,7 +4,7 @@ import { mswHttp } from '@/mocks/shared';
 import type { SuccessResponse } from '@/services/shared';
 import type { PostBusinessRegistrationNumberResponseDto } from '@/types/onboarding/store-register';
 
-export const storeRegisterHandler = [
+const postHandler = [
   mswHttp.post('/api/stores/business/verify', () => {
     return HttpResponse.json<
       SuccessResponse<PostBusinessRegistrationNumberResponseDto>
@@ -12,10 +12,10 @@ export const storeRegisterHandler = [
       {
         success: true,
         data: { businessAuthToken: 'mock-business-auth-token' },
-        message: '사업자 인증 성공',
-        errorCode: '',
       },
       { status: 201 },
     );
   }),
 ];
+
+export const storeRegisterHandler = [...postHandler];
