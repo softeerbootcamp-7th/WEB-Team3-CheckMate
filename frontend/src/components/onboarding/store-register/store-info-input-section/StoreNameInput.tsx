e2615f9c39ab5315bf1600c994c7f0a2276e8a45
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-04 14:53:46 +0900

    feat: 매장 이름 입력 컴포넌트 추가

--- frontend/src/components/onboarding/store-register/store-info-input-section/StoreNameInput.tsx
+++ frontend/src/components/onboarding/store-register/store-info-input-section/StoreNameInput.tsx
@@ -1,11 +1,18 @@
 import { Input } from '@/components/shared';
+import { useStoreName } from '@/hooks/onboarding/store-register';
 
 export const StoreNameInput = () => {
+  const { combineRefCallback, error, field } = useStoreName();
+
   return (
     <Input
       label="매장명"
       description="서비스 안에서 표시될 이름이에요"
       placeholder="매장명을 입력해주세요"
+      isError={!!error}
+      errorMessage={error?.message}
+      {...field}
+      ref={combineRefCallback}
     />
   );
 };
