0ef1ac864fa3a648e10651fa604c5fff077a77eb
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-05 01:04:59 +0900

    feat: 매장 영업시간 입력 페이지에서 다음 스텝으로 넘어갈지 pos 연동 페이지로 이동할지에 대한 로직 추가

--- frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
+++ frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
@@ -1,4 +1,5 @@
 import { FormProvider, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 import {
   STORE_REGISTER_FORM_DEFAULT_VALUE,
@@ -13,16 +14,28 @@ import {
 } from '../store-register-form-content';
 
 export const StoreRegisterForm = () => {
+  const navigate = useNavigate();
   const methods = useForm<StoreRegisterFormType>({
     mode: 'all',
     defaultValues: STORE_REGISTER_FORM_DEFAULT_VALUE,
   });
   const { currentStep, moveNextStep } = useStoreRegisterStepContext();
 
-  const handleSubmit = methods.handleSubmit(() => {
-    if (currentStep < STORE_REGISTER_STEP.SALES_CLOSING_TIME) {
+  const handleSubmit = methods.handleSubmit((data) => {
+    if (currentStep < STORE_REGISTER_STEP.STORE_BUSINESS_HOURS) {
       moveNextStep();
       return;
+    }
+
+    if (currentStep === STORE_REGISTER_STEP.STORE_BUSINESS_HOURS) {
+      const has24BusinessHour = data.businessHours.some(
+        (businessHour) => businessHour.is24,
+      );
+      if (has24BusinessHour) {
+        moveNextStep();
+        return;
+      }
+      navigate('../pos', { replace: true });
     }
   });
 
