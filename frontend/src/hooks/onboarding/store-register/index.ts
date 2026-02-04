898c2b31b0d4646df67695b02e11b6c6f6dca0d6
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-04 15:02:46 +0900

    feat: 배럴 파일 추가

--- frontend/src/hooks/onboarding/store-register/index.ts
+++ frontend/src/hooks/onboarding/store-register/index.ts
@@ -1,3 +1,5 @@
 export { useStoreRegisterStepContext } from './useStoreRegisterStepContext';
 export { useBusinessRegistration } from './useBusinessRegistration';
 export { useVerifyBusinessRegistrationNumber } from './useVerifyBusinessRegistrationNumber';
+export { useStoreName } from './useStoreName';
+export { useStoreAddress } from './useStoreAddress';
