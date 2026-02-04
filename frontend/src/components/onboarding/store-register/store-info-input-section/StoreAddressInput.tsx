99708a6788cc69b419cf04b2bb302318ed498e47
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-04 14:56:38 +0900

    feat: 주소 입력 컴포넌트 추가

--- frontend/src/components/onboarding/store-register/store-info-input-section/StoreAddressInput.tsx
+++ frontend/src/components/onboarding/store-register/store-info-input-section/StoreAddressInput.tsx
@@ -1,8 +1,18 @@
 import { Input } from '@/components/shared';
+import { useStoreAddress } from '@/hooks/onboarding/store-register';
 
 import { AddressSearchButton } from './AddressSearchButton';
 
 export const StoreAddressInput = () => {
+  const {
+    zonecodeField,
+    addressField,
+    errorMessage,
+    isError,
+    addressSearchButtonRef,
+    handleFocusNextStepButton,
+  } = useStoreAddress();
+
   return (
     <div className="flex flex-col gap-3">
       <div className="relative flex gap-3">
@@ -10,12 +20,24 @@ export const StoreAddressInput = () => {
           label="매장 주소"
           description="날씨 기반 매출 분석을 위한 정보예요"
           placeholder="우편번호"
-          inputClassName="w-[14.1875rem]"
+          inputClassName="w-59"
+          {...zonecodeField}
+          isError={isError}
+          onClick={handleFocusNextStepButton}
+          readOnly
+          tabIndex={-1}
         />
-        <AddressSearchButton />
+        <AddressSearchButton ref={addressSearchButtonRef} />
       </div>
-      <Input placeholder="도로명 주소" disabled />
-      <Input placeholder="상세 주소" disabled />
+      <Input
+        placeholder="도로명 주소"
+        {...addressField}
+        isError={isError}
+        errorMessage={errorMessage}
+        onClick={handleFocusNextStepButton}
+        readOnly
+        tabIndex={-1}
+      />
     </div>
   );
 };
