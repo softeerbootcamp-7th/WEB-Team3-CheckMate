53f43da8af7b88b18f7ccbd2c8ce0f628ce4538a
minseon <mskwon0601@gmail.com>
2026-02-04 14:45:48 +0900

    feat: 식자재 입력에서 사용되는 기본 타입 정의

---
+++ frontend/src/types/ingredient/index.ts
@@ -0,0 +1,1 @@
+export { type Ingredient, type FormValues } from './ingredient';
---
+++ frontend/src/types/ingredient/ingredient.ts
@@ -0,0 +1,12 @@
+export interface Ingredient {
+  id: string;
+  name: string;
+  // input은 값들을 보통 string으로 다룸
+  // -> 그래야 새로운 필드 추가할 때 빈 문자열로 초기화 해 placeholder 뜨게 할 수 있음(숫자는 0으로 초기화 하거나(그러면 placeholder 안뜸) null 허용 해야함)
+  amount: string;
+  unit: string;
+}
+
+export interface FormValues {
+  ingredients: Ingredient[];
+}
