24401e98ff74ec0c317b3faf3f2337a9ea18e9a8
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-05 16:04:47 +0900

    feat: console.error 제거

--- frontend/src/pages/root-error-fallback/RootErrorFallback.tsx
+++ frontend/src/pages/root-error-fallback/RootErrorFallback.tsx
@@ -1,20 +1,17 @@
 import { Navigate, useRouteError } from 'react-router-dom';
 
-import { ApiError } from '@/services/shared';
+import { isApiError } from '@/services/shared';
 
 // TODO: 에러 페이지 디자인 추가
 export const RootErrorFallback = () => {
   const error = useRouteError();
 
-  if (error instanceof ApiError) {
+  if (isApiError(error)) {
     switch (error.status) {
       case 401:
         return <Navigate to="/sign-in" replace />;
     }
   }
-
-  console.error(error);
-
   const errorMessage = error instanceof Error ? error.message : 'Unknown error';
 
   return <div>에러 발생 : {errorMessage}</div>;
