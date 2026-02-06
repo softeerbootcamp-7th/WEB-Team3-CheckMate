422a734c8a292fc311ff6e26573a6cf96d7bb567
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-06 10:36:49 +0900

    feat: 주석 수정

--- frontend/src/types/onboarding/store-register/storeRegisterForm.ts
+++ frontend/src/types/onboarding/store-register/storeRegisterForm.ts
@@ -2,8 +2,8 @@ type DayOfWeek = 'MON' | 'TUE' | 'WED' | 'THU' | 'FRI' | 'SAT' | 'SUN';
 
 interface BusinessHour {
   dayOfWeek: DayOfWeek;
-  openTime?: string; // 00:00 ~ 24:00
-  closeTime?: string; // 00:00 ~ 24:00
+  openTime?: string; // 00:00 ~ 23:30
+  closeTime?: string; // 00:00 ~ 23:30
   is24?: boolean;
   closed?: boolean;
   // client에만 있는 상태, 마감 시간이 다음날 00:00 이후인 경우 체크
