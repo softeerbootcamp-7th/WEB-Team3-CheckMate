822f19a33fe532eaa127c83a8434c2ee54270bf0
lwjmcn <clarecse02@gmail.com>
2026-02-06 11:53:33 +0900

    style: 버튼에 폰트 스타일링 Important 처리

--- frontend/src/components/dashboard/dashboard-tabs-dialog/DialogCancelButton.tsx
+++ frontend/src/components/dashboard/dashboard-tabs-dialog/DialogCancelButton.tsx
@@ -8,7 +8,7 @@ export const DialogCancelButton = ({ onClick }: DialogCancelButtonProps) => {
   return (
     <Button
       onClick={onClick}
-      className="rounded-200 text-grey-700 body-medium-bold w-20 px-350 py-200"
+      className="rounded-200 text-grey-700 body-medium-bold! w-20 px-350 py-200"
     >
       취소
     </Button>
--- frontend/src/components/dashboard/dashboard-tabs-dialog/DialogSaveButton.tsx
+++ frontend/src/components/dashboard/dashboard-tabs-dialog/DialogSaveButton.tsx
@@ -7,7 +7,7 @@ export const DialogSaveButton = ({ onClick }: DialogSaveButtonProps) => {
   return (
     <Button
       onClick={onClick}
-      className="bg-grey-900 rounded-200 text-grey-50 body-medium-bold w-20 px-350 py-200"
+      className="bg-grey-900 rounded-200 text-grey-50 body-medium-bold! w-20 px-350 py-200 pt-250"
     >
       저장
     </Button>
