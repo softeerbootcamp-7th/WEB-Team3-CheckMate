35fce28ba88a4eb5760038c2c4b892b46dd72a7c
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-07 20:49:30 +0900

    feat: 메뉴 판매 패턴 컴포넌트 추가

--- frontend/src/pages/menu-page/MenuPage.tsx
+++ frontend/src/pages/menu-page/MenuPage.tsx
@@ -1,8 +1,10 @@
 import { PopularMenuOverview } from '@/components/menu';
+import { MenuSalesPatternOverview } from '@/components/menu/menu-salse-pattern';
 export const MenuPage = () => {
   return (
     <div className="flex size-full flex-col gap-12 py-32.5">
       <PopularMenuOverview />
+      <MenuSalesPatternOverview />
     </div>
   );
 };
