6cebe8541cdd373ca83e3cb69648fe9d0f271385
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-08 02:12:48 +0900

    feat: 식재료 소진량 랭킹 관련 카드 컴포넌트 추가

---
+++ frontend/src/components/menu/ingredient-consumption/IngredientConsumptionRankCard.tsx
@@ -0,0 +1,38 @@
+import { INGREDIENT_CONSUMPTION_RANK } from '@/constants/menu';
+import { ROUTE_PATHS } from '@/constants/shared';
+import { ingredientConsumptionRankItems } from '@/mocks/data/menu';
+
+import { LoadMoreDataButton, MenuAnalysisCard } from '../shared';
+
+import { IngredientConsumptionRankList } from './IngredientConsumptionRankList';
+
+export const IngredientConsumptionRankCard = () => {
+  const ingredientConsumptionRank1to5 = ingredientConsumptionRankItems.slice(
+    0,
+    INGREDIENT_CONSUMPTION_RANK.MAX_DISPLAYED_RANK_ITEMS_1_TO_5,
+  );
+
+  const ingredientConsumptionRank6to10 = ingredientConsumptionRankItems.slice(
+    INGREDIENT_CONSUMPTION_RANK.MAX_DISPLAYED_RANK_ITEMS_1_TO_5,
+    INGREDIENT_CONSUMPTION_RANK.MAX_DISPLAYED_RANK_ITEMS_6_TO_10,
+  );
+  return (
+    <MenuAnalysisCard
+      aria-label="식재료 소진량 랭킹"
+      title="식재료 소진량 랭킹"
+      className="flex h-80 min-w-0 flex-1 flex-col gap-7.5"
+    >
+      <div className="flex gap-5">
+        <IngredientConsumptionRankList
+          ingredientConsumptionRank={ingredientConsumptionRank1to5}
+        />
+        <IngredientConsumptionRankList
+          ingredientConsumptionRank={ingredientConsumptionRank6to10}
+        />
+      </div>
+      <LoadMoreDataButton
+        path={`./${ROUTE_PATHS.ANALYSIS.INGREDIENT_CONSUMPTION_RANK}`}
+      />
+    </MenuAnalysisCard>
+  );
+};
