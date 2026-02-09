6a0d209e2ceff228d853d42a0988b1728c1ec597
minseon <mskwon0601@gmail.com>
2026-02-09 14:31:13 +0900

    chore: 통신 중/ 통신 실패 상황에서 뒷 카드 배경이 되어줄 StateWrapper 사용

--- frontend/src/components/setting/setting-my-store-info/SettingMyStoreInfo.tsx
+++ frontend/src/components/setting/setting-my-store-info/SettingMyStoreInfo.tsx
@@ -1,8 +1,10 @@
 import { FormProvider } from 'react-hook-form';
 
+import { Spinner } from '@/components/shared';
 import { useStoreRegisterForm } from '@/hooks/onboarding/store-register';
 import { useSettingMyStoreInfo } from '@/hooks/setting';
 
+import { StateWrapper } from '../../shared/state-wrapper';
 import { SettingMyStoreBusinessHours } from '../setting-my-store-business-hours';
 import { SettingMyStoreName } from '../setting-my-store-name';
 import { SettingMyStoreSalesClosingTime } from '../setting-my-store-sales-closing-time';
@@ -12,24 +14,26 @@ export const SettingMyStoreInfo = () => {
   const { methods } = useStoreRegisterForm();
   if (isPending) {
     return (
-      <section className="rounded-400 bg-special-card-bg flex h-129 w-260 items-center justify-center px-8 pt-6 pb-9">
-        Loading...
-      </section>
+      <StateWrapper width={1040} height={590}>
+        <Spinner className="size-8" />
+      </StateWrapper>
     );
   }
   if (error || !data) {
     return (
-      <section className="rounded-400 bg-special-card-bg flex h-129 w-260 items-center justify-center px-8 pt-6 pb-9">
-        매장 정보를 불러오지 못했습니다.
-      </section>
+      <StateWrapper width={1040} height={590}>
+        <span>매장 정보를 불러오지 못했습니다. 다시 시도해 주세요</span>
+      </StateWrapper>
     );
   }
   return (
     <FormProvider {...methods}>
       <section className="rounded-400 bg-special-card-bg w-260 px-8 pt-6 pb-9">
-        <span className="title-small-bold text-grey-900">내 매장 정보</span>
+        <span className="title-small-bold text-grey-900 w-full">
+          내 매장 정보
+        </span>
         <div className="bg-grey-300 mt-6 h-0.5 w-244.5" />
-        <div className="pointer-events-none mt-4 grid grid-cols-2">
+        <div className="pointer-events-none mt-4 grid w-full grid-cols-2">
           <div className="flex w-95 flex-col gap-12.5">
             <SettingMyStoreName storeName={data.storeName} />
             <SettingMyStoreSalesClosingTime
