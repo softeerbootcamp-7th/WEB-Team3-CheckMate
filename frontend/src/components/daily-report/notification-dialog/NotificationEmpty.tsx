b0cf68aeb146a26c4e0859c9f5656ec22f541bc3
lwjmcn <clarecse02@gmail.com>
2026-02-09 16:41:29 +0900

    chore: s3 이미지 업로드 및 연결

--- frontend/src/components/daily-report/notification-dialog/NotificationEmpty.tsx
+++ frontend/src/components/daily-report/notification-dialog/NotificationEmpty.tsx
@@ -1,8 +1,13 @@
+import { CDN_BASE_URL } from '@/constants/shared/cdnBaseUrl';
+
 export const NotificationEmpty = () => {
   return (
     <div className="mt-4 flex h-85 flex-col items-center justify-center gap-2">
       {/* 임시 이미지 주소 */}
-      <object data="images.svg" className="bg-grey-300 size-13" />
+      <object
+        data={`${CDN_BASE_URL}/assets/images/alarm_none.svg`}
+        className="size-13"
+      />
       <p className="text-grey-900 body-small-medium">
         현재 확인할 알림이 없어요.
       </p>
