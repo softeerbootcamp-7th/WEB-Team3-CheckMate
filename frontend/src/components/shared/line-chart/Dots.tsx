7bedd368979b786715b26699c47e9b860693c20e
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-10 01:35:26 +0900

    feat: line chart data point 컴포넌트 추가

---
+++ frontend/src/components/shared/line-chart/Dots.tsx
@@ -0,0 +1,81 @@
+import { LINE_CHART } from '@/constants/shared';
+import type { LineChartSeries } from '@/types/shared';
+
+import { Tooltip, TooltipContent, TooltipTrigger } from '../shadcn-ui';
+
+interface DotsProps {
+  series: LineChartSeries;
+  activeTooltip: boolean;
+  tooltipContent: (...args: string[]) => string;
+  cordinate: (number | null)[][];
+  color: string;
+}
+
+export const Dots = ({
+  series,
+  activeTooltip,
+  tooltipContent,
+  cordinate,
+  color,
+}: DotsProps) => {
+  const { DOT_RADIUS } = LINE_CHART;
+
+  const filteredCordinate: number[][] = cordinate.filter(
+    (point): point is number[] => point[1] !== null,
+  );
+
+  if (!activeTooltip) {
+    return (
+      <>
+        {filteredCordinate.map(([x, y], index) => (
+          <circle
+            key={index}
+            cx={x}
+            cy={y}
+            r={DOT_RADIUS}
+            fill={color}
+            stroke="none"
+            role="graphics-symbol"
+            tabIndex={0}
+            aria-label={`${series.data.mainX[index].amount} ${series.data.mainX[index].unit}`}
+            style={{
+              transition: 'cx 0.5s ease-in-out, cy 0.5s ease-in-out',
+            }}
+          />
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <>
+      {filteredCordinate.map(([x, y], index) => {
+        const tooltipContentText = tooltipContent(
+          `${series.data.mainY[index].amount}${series.data.mainY[index].unit}`,
+          `${series.data.subY[index]?.amount}${series.data.subY[index]?.unit}`,
+        );
+        return (
+          <Tooltip key={index}>
+            <TooltipTrigger asChild>
+              <circle
+                cx={x}
+                cy={y}
+                r={DOT_RADIUS}
+                fill={color}
+                role="graphics-symbol"
+                stroke="none"
+                className="hover:brightness-75 hover:saturate-200"
+                tabIndex={0}
+                aria-label={tooltipContentText}
+                style={{
+                  transition: 'cx 0.5s ease-in-out, cy 0.5s ease-in-out',
+                }}
+              />
+            </TooltipTrigger>
+            <TooltipContent>{tooltipContentText}</TooltipContent>
+          </Tooltip>
+        );
+      })}
+    </>
+  );
+};
