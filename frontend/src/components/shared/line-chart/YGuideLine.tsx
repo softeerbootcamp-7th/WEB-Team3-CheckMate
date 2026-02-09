a5e2fe2598e833022ab40c46686ead2680dfbcbf
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-10 01:33:43 +0900

    feat: y축 가이드라인 컴포넌트 추가

---
+++ frontend/src/components/shared/line-chart/YGuideLine.tsx
@@ -0,0 +1,46 @@
+import { LINE_CHART } from '@/constants/shared';
+
+interface YGuideLineProps {
+  svgRect: DOMRect | null;
+  adjustedHeight: number;
+  yGuideLineCount: number;
+}
+
+export const YGuideLine = ({
+  svgRect,
+  adjustedHeight,
+  yGuideLineCount,
+}: YGuideLineProps) => {
+  if (svgRect === null) {
+    return null;
+  }
+
+  const { GUIDE_LINE_STROKE_WIDTH } = LINE_CHART;
+
+  const intervalY = adjustedHeight / yGuideLineCount;
+
+  const cordinateYGuideLine = Array.from({ length: yGuideLineCount }).reduce<
+    number[][]
+  >(
+    (acc, _, index) => [
+      ...acc,
+      [0, index * intervalY + GUIDE_LINE_STROKE_WIDTH / 2],
+    ],
+    [],
+  );
+
+  const pathD = cordinateYGuideLine
+    .map(([x, y]) => `M ${x} ${y} h ${svgRect.width}`)
+    .join(' ');
+
+  return (
+    <path
+      d={pathD}
+      strokeWidth={GUIDE_LINE_STROKE_WIDTH}
+      stroke="currentColor"
+      strokeDasharray="4 4"
+      className="text-grey-300"
+      aria-hidden="true"
+    />
+  );
+};
