782572a4ed3afc9b56de99d58870199b895bab17
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-10 17:12:40 +0900

    feat: onboarding layout footer 추가 및 grid template 관련 클래스 추가

--- frontend/src/components/shared/onboarding-layout/OnboardingLayout.tsx
+++ frontend/src/components/shared/onboarding-layout/OnboardingLayout.tsx
@@ -1,18 +1,39 @@
 import type { PropsWithChildren } from 'react';
 
 export const OnboardingLayout = ({ children }: PropsWithChildren) => {
-  return <div className="grid size-full grid-cols-[300px_1fr]">{children}</div>;
+  return <div className="onboarding-layout">{children}</div>;
 };
 
 const OnboardingSidebar = ({ children }: PropsWithChildren) => {
   return (
-    <aside className="bg-special-dashboard-bg size-full">{children}</aside>
+    <aside
+      className="bg-special-dashboard-bg size-full"
+      style={{ gridArea: 'sidebar' }}
+    >
+      {children}
+    </aside>
   );
 };
 
 const OnboardingMain = ({ children }: PropsWithChildren) => {
-  return <main className="bg-special-card-bg size-full">{children}</main>;
+  return (
+    <main className="bg-special-card-bg size-full" style={{ gridArea: 'main' }}>
+      {children}
+    </main>
+  );
+};
+
+const OnboardingFooter = ({ children }: PropsWithChildren) => {
+  return (
+    <footer
+      className="bg-special-card-bg size-full"
+      style={{ gridArea: 'footer' }}
+    >
+      {children}
+    </footer>
+  );
 };
 
 OnboardingLayout.Sidebar = OnboardingSidebar;
 OnboardingLayout.Main = OnboardingMain;
+OnboardingLayout.Footer = OnboardingFooter;
