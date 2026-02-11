83cad8904e2656f1f04b5f98d945341c4371b714
Yejin Lee <114637188+lwjmcn@users.noreply.github.com>
2026-02-11 15:16:39 +0900

    [FE] Loader가 MSW보다 먼저 실행되는 에러 수정 (#253)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,22 +5,12 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
-const enableMocking = async () => {
-  if (import.meta.env.MODE !== 'development') {
-    return;
-  }
-  const { worker } = await import('./mocks/browser.ts');
-  return worker.start();
-};
-
 const root = document.getElementById('root');
 
-enableMocking().then(() => {
-  if (root) {
-    createRoot(root).render(
-      <StrictMode>
-        <App />
-      </StrictMode>,
-    );
-  }
-});
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
