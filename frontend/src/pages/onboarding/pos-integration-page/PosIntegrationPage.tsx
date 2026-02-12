58efa23381665f0560620fd87876255961fa72ac
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-12 15:08:59 +0900

    feat: 포스 연동 페이지 SSE 연결 및 포스 연동 절차에 따른 페이지 조건부 노출 로직 추가

--- frontend/src/pages/onboarding/pos-integration-page/PosIntegrationPage.tsx
+++ frontend/src/pages/onboarding/pos-integration-page/PosIntegrationPage.tsx
@@ -1,16 +1,20 @@
-import { PosIntegrationGuideSection } from '@/components/onboarding/pos-integration';
+import { PosIntegrationMain } from '@/components/onboarding/pos-integration';
 import { OnboardingSidebar } from '@/components/onboarding/shared';
 import { OnboardingLayout } from '@/components/shared';
+import { usePosIntegration } from '@/hooks/onboarding/pos-integration';
 
 // TODO: POS 연동 상태 따른 section 조건부 렌더링 필요
+
 export const PosIntegrationPage = () => {
+  const { event } = usePosIntegration();
+
   return (
     <OnboardingLayout>
       <OnboardingLayout.Sidebar>
         <OnboardingSidebar />
       </OnboardingLayout.Sidebar>
       <OnboardingLayout.Main>
-        <PosIntegrationGuideSection />
+        <PosIntegrationMain eventData={event?.data} />
       </OnboardingLayout.Main>
     </OnboardingLayout>
   );
