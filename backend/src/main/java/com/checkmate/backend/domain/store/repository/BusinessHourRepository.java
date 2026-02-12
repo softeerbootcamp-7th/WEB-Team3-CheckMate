e3220e86751f09d61cd64f0d32acd7afb663029d
jho7535 <jho7535@naver.com>
2026-02-12 23:03:21 +0900

    feat: 리포트 발행 대상 매장 조회 메서드 추가
    - 일반 매장 대상 조회 쿼리 메서드 추가
    - 24시간 매장 대상 조회 쿼리 메서드 추가"

--- backend/src/main/java/com/checkmate/backend/domain/store/repository/BusinessHourRepository.java
+++ backend/src/main/java/com/checkmate/backend/domain/store/repository/BusinessHourRepository.java
@@ -14,4 +14,27 @@ public interface BusinessHourRepository extends JpaRepository<BusinessHour, Long
 
     @Query("select bh from BusinessHour bh" + " where bh.store.id =:storeId")
     List<BusinessHour> findBusinessHoursByStoreId(@Param("storeId") Long storeId);
+
+    @Query(
+            "SELECT bh FROM BusinessHour bh JOIN FETCH bh.store "
+                    + "WHERE bh.closeTime = :currentTime "
+                    + "AND bh.closed = false "
+                    + "AND ("
+                    + "    (bh.day = :today AND bh.closesNextDay = false) "
+                    + "    OR "
+                    + "    (bh.day = :yesterday AND bh.closesNextDay = true)"
+                    + ")")
+    List<BusinessHour> findNormalClosingStores(
+            @Param("today") String today,
+            @Param("yesterday") String yesterday,
+            @Param("currentTime") String currentTime);
+
+    @Query(
+            "SELECT bh FROM BusinessHour bh JOIN FETCH bh.store s "
+                    + "WHERE bh.closed = false "
+                    + "AND bh.open24Hours = true "
+                    + "AND bh.day = :today "
+                    + "AND s.salesClosingHour = :currentHour")
+    List<BusinessHour> find24HClosingStores(
+            @Param("today") String today, @Param("currentHour") Integer currentHour);
 }
