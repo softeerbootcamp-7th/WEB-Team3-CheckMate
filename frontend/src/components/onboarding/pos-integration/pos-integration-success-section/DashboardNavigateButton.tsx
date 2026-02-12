998210d3c9ec7281b28dadc74e38e90aa853e068
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-12 15:25:33 +0900

    feat: 포스 연동 성공 시 회원정보 refetch 로직 추가

--- frontend/src/components/onboarding/pos-integration/pos-integration-success-section/DashboardNavigateButton.tsx
+++ frontend/src/components/onboarding/pos-integration/pos-integration-success-section/DashboardNavigateButton.tsx
@@ -6,7 +6,6 @@ export const DashboardNavigateButton = () => {
   return (
     <Button
       className="rounded-150 bg-brand-main text-grey-50 title-small-semibold! flex h-14 w-55 items-center justify-center"
-      type="button"
       asChild
     >
       <Link to="/dashboard" replace>
