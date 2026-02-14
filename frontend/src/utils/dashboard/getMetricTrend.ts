5ee98bb13235ceb240633b51d82abe85efcdad01
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-14 17:26:02 +0900

    feat: 최대 최소가 0인 경우 추가

--- frontend/src/utils/dashboard/getMetricTrend.ts
+++ frontend/src/utils/dashboard/getMetricTrend.ts
@@ -11,6 +11,16 @@ export const getMetricTrend = ({
   minValue,
   maxValue,
 }: GetMetricTrendArgs) => {
+  if (!maxValue && !minValue) {
+    if (comparisonAmount > 0) {
+      return METRIC_TREND.UP;
+    } else if (comparisonAmount < 0) {
+      return METRIC_TREND.DOWN;
+    } else {
+      return METRIC_TREND.SAME;
+    }
+  }
+
   if (comparisonAmount >= maxValue) {
     return METRIC_TREND.UP;
   } else if (comparisonAmount <= minValue) {
