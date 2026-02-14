1addde0a3c124328c5964c8ebaeefc88b01db650
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-15 03:26:21 +0900

    feat: 주문수단별 매출 추가

--- frontend/src/constants/sales/index.ts
+++ frontend/src/constants/sales/index.ts
@@ -1,6 +1,12 @@
-export { SALES_SOURCE_COLORS } from './salesSource';
+export {
+  SALES_SOURCE_COLORS,
+  isSalesSourceType,
+  SALES_SOURCE,
+} from './salesSource';
 export type { SalesSourceType } from './salesSource';
 export { SALES_UNIT } from './salesUnit';
 export { REAL_SALES } from './realSales';
 export { ORDER_COUNT } from './orderCount';
 export { AVERAGE_PRICE } from './averagePrice';
+export { SALES_TYPE } from './salesType';
+export { ORDER_METHOD } from './orderMethod';
