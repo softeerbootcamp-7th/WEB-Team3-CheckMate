fa99942afef9aab15d644ee52925251132679478
lwjmcn <clarecse02@gmail.com>
2026-02-16 00:56:47 +0900

    feat: 그리드 셀 크기 계산을 위한 useGridCellSize 추가
    - 대시보드에서 카드의 위치와 크기를 계산하는 훅을 추가함
    - 카드를 absolute 포지션으로 배치하고, 마우스 위치를 통해 그리드 셀을 도출해내는 로직을 위해 필요

--- frontend/src/hooks/dashboard/index.ts
+++ frontend/src/hooks/dashboard/index.ts
@@ -2,3 +2,4 @@ export { useDashboardTabsContext } from './useDashboardTabsContext';
 export { useDashboardTabsDialog } from './useDashboardTabsDialog';
 export { useEditCard } from './useEditCard';
 export { useEditCardContext } from './useEditCardContext';
+export { useGridCellSize } from './useGridCellSize';
