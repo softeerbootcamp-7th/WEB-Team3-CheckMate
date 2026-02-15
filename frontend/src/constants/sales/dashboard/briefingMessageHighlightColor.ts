fa1fa4486c5b372bb4cdcbf132628c41a6f59e3d
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-15 19:12:25 +0900

    chore: font-bold 추가

--- frontend/src/constants/sales/dashboard/briefingMessageHighlightColor.ts
+++ frontend/src/constants/sales/dashboard/briefingMessageHighlightColor.ts
@@ -1,7 +1,7 @@
 export const BRIEFING_MESSAGE_HIGHLIGHT_COLOR = {
   primary: 'text-brand-main font-bold',
   negative: 'text-others-negative font-bold',
-  default: 'text-grey-500',
+  default: 'text-grey-500 font-bold',
 } as const;
 
 export type BriefingMessageHighlightColor =
--- frontend/src/constants/sales/dashboard/index.ts
+++ frontend/src/constants/sales/dashboard/index.ts
@@ -2,9 +2,3 @@ export {
   BRIEFING_MESSAGE_HIGHLIGHT_COLOR,
   type BriefingMessageHighlightColor,
 } from './briefingMessageHighlightColor';
-export { AVERAGE_PRICE } from './averagePrice';
-export { ORDER_COUNT } from './orderCount';
-export { REAL_SALES } from './realSales';
-export { ORDER_METHOD } from './orderMethod';
-export { PAYMENT_METHOD } from './paymentMethod';
-export { SALES_TYPE } from './salesType';
