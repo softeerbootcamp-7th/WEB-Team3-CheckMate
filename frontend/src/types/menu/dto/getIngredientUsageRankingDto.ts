5cec2f1b0d32cf40e0b95129299701accc8560f6
minseon <mskwon0601@gmail.com>
2026-02-15 18:55:38 +0900

    chore: 식자재별 소진량 랭킹 카드 DTO 주석 수정

--- frontend/src/types/menu/dto/getIngredientUsageRankingDto.ts
+++ frontend/src/types/menu/dto/getIngredientUsageRankingDto.ts
@@ -7,7 +7,7 @@ export interface IngredientUsage {
   baseUnit: string;
 }
 
-// 백엔드에서 보내주는 식자재별 소진량 DTO -> 매출별 랭킹에 사용
+// 백엔드에서 보내주는 식자재별 소진량 DTO -> 식자재별 소진량 랭킹에 사용
 export interface GetIngredientUsageRankingResponseDto {
   items: IngredientUsage[];
 }
