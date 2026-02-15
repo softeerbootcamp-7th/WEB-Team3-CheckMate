62ec7266d9dd5b634cf452990b213e5609f8d7ca
minseon <mskwon0601@gmail.com>
2026-02-15 13:23:26 +0900

    feat: 식자재 소진량 매출 랭킹 카드

--- frontend/src/components/dashboard/dashboard-menu/DashboardIngredientRankingCard.tsx
+++ frontend/src/components/dashboard/dashboard-menu/DashboardIngredientRankingCard.tsx
@@ -1,14 +1,30 @@
 import { useNavigate } from 'react-router-dom';
 
 import { DefaultCardWrapper } from '@/components/shared';
-import { DASHBOARD_METRIC_CARDS } from '@/constants/dashboard';
+import {
+  DASHBOARD_METRIC_CARDS,
+  DASHBOARD_METRICS,
+  type ExtractCardCodes,
+} from '@/constants/dashboard';
+import { CDN_BASE_URL } from '@/constants/shared';
 import { dashboardMenuIngredientRankItems } from '@/mocks/data/dashboard';
 import { cn } from '@/utils/shared';
 
 import { DashboardMenuRankItem } from './DashboardMenuRankItem';
 
-export const DashboardIngredientRankingCard = () => {
-  const cardCode = 'MNU_04_01';
+// 식자재 소진량 매출 랭킹 카드 코드
+type IngredientConsumptionRankCardCode = ExtractCardCodes<
+  typeof DASHBOARD_METRICS.MENU.sections.INGREDIENT_CONSUMPTION_RANK.items.INGREDIENT_CONSUMPTION_RANK
+>;
+
+// 메뉴분석 > 식자재 소진량 매출 랭킹
+interface DashboardIngredientRankingCardProps {
+  cardCode: IngredientConsumptionRankCardCode;
+}
+
+export const DashboardIngredientRankingCard = ({
+  cardCode,
+}: DashboardIngredientRankingCardProps) => {
   const cardInfo = DASHBOARD_METRIC_CARDS[cardCode];
 
   const navigate = useNavigate();
@@ -41,7 +57,12 @@ export const DashboardIngredientRankingCard = () => {
         ))
       ) : (
         <div className="flex flex-col items-center">
-          <div className="flex flex-col items-center gap-2">
+          <div className="flex flex-col items-center gap-1">
+            <img
+              src={`${CDN_BASE_URL}/assets/images/empty_ingridient.svg`}
+              alt="등록된 식자재 없음"
+              className="size-18"
+            />
             <span className="body-large-bold">식재료 미등록</span>
             <span className="body-small-medium text-grey-700 text-center">
               자동으로 식재료 파악하고,
